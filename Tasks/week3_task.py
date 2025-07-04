d4914d5294fbf2dbe4675744ed09c1bbe48f3180
Esther Erommonsele <eerommonsele@yahoo.com>
2025-07-04 10:56:59 -0400

    Initial commit

---
+++ Tasks/week3_task.py
@@ -0,0 +1,28 @@
+#First variable
+num1 = 10
+#Second variable
+num2 = 5
+#Result of summation
+result = num1 + num2
+print("Sum of 10 and 5:", result)
+
+#Calculate and print the result of 25 - 10
+x = 25
+y = 10
+result1 = x - y
+#Result of subtraction task
+print(f"subtraction of 10 from 25:", result1)
+
+#Calculate and print the result of 7 * 4.
+a = 7
+b = 4
+#Result of product of 7 and 4
+result2 = a * b
+print(f"product of 7 and 4:", result2)
+
+#Calculate and print the result of 20 / 4.
+c = 20
+d = 4
+#Result of division of 20 by 4
+result3 = c / d
+print(f"division of 20 by 4:", result3)
